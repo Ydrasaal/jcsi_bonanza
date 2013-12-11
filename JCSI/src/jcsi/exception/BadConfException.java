2dafb7859d4ac5ac67ee796c4443667fabb87271
Netikaz <Netikaz.Tek@gmail.com>
2013-12-11 03:42:27 +0100

    Exception merged

--- JCSI/src/jcsi/exception/BadConfException.java
+++ JCSI/src/jcsi/exception/BadConfException.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package jcsi.exception;
 
 import jcsi.log.UniLogger;
@@ -12,23 +11,4 @@ public class BadConfException extends JCSIException {
 		UniLogger log = UniLogger.getInstance();
 		log.error("Configuration failure ! Check the configuration file format.");
 	}
-
-=======
-package jcsi.exception;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-public class BadConfException extends JCSIException {
-
-	private static final long serialVersionUID = 1L;
-
-	public BadConfException() {
-		super();
-		Logger log = Logger.getLogger(this.getClass().getName());
-		log.setLevel(Level.SEVERE);
-		log.severe("Configuration failure ! Check the configuration file format.");
-	}
-
->>>>>>> 3dd9800286243c63138a4063962f4b64fe181fbe
 }
