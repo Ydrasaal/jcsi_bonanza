320ba3ce820ee5f95b1f712dcc03ea8dabea62bc
Ydrasaal <leo.chazal@epitech.eu>
2013-12-11 13:54:17 +0100

    FINAL

--- JCSI/src/jcsi/bootstrap/Main.java
+++ JCSI/src/jcsi/bootstrap/Main.java
@@ -8,6 +8,7 @@ import jcsi.dataAccess.DAO.CartDAO;
 import jcsi.dataAccess.DAO.CategoryDAO;
 import jcsi.dataAccess.DAO.ClientDAO;
 import jcsi.dataAccess.DAO.ProductDAO;
+import jcsi.exception.JCSIException;
 import jcsi.gui.WindowView;
 import jcsi.log.UniLogger;
 import jcsi.orm.entity.Cart;
@@ -27,45 +28,48 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-
-		WindowView view = new WindowView();
-		
-		view.setSize(800, 600);
-		view.setLocation(150, 100);
-		view.setVisible(true);
-		
-		UniLogger.INSTANCE.setView(view);
-		
-		Client c1 = new Client();
-		c1.setEmail("tango@charlie.plon");
-		c1.setFirstName("Serge");
-		c1.setLastName("Zitrou");
-		c1.setPhone("18");
-		
-		Coordinates coo = new Coordinates();
-		coo.setAddress("77 avenue du Ciel");
-		coo.setCity("Farmville");
-		coo.setCountry("Donnmork");
-		c1.setCoordinates(coo);
-
-		Cart c = new Cart();
-		c1.addCart(c);
-		
-		Product p1 = new Product();
-		p1.setName("Gouigoui rebondissant");
-		p1.setPrice(.5);
-		p1.setCategory(CategoryDAO.INSTANCE.getByName("Truc en plastoc"));
-		c.addContent(new Content(p1, 2));
-		c.addContent(new Content(ProductDAO.INSTANCE.getByName("Caribou mécanique"), 6));
-		
-		CRUDManager.createOrUpdate(c1);
-		
-		
-		System.out.println(ClientDAO.INSTANCE.getAllByLastName("Zorbier").toString());
-		
-		System.out.println(CartDAO.INSTANCE.getAllByPaymentState(false).toString());
-
-		System.out.println("All is well.");
+		try {
+			WindowView view = new WindowView();
+			
+			view.setSize(800, 600);
+			view.setLocation(150, 100);
+			view.setVisible(true);
+			
+			UniLogger.INSTANCE.setView(view);
+			
+			Client c1 = new Client();
+			c1.setEmail("tango@charlie.plon");
+			c1.setFirstName("Serge");
+			c1.setLastName("Zitrou");
+			c1.setPhone("18");
+			
+			Coordinates coo = new Coordinates();
+			coo.setAddress("77 avenue du Ciel");
+			coo.setCity("Farmville");
+			coo.setCountry("Donnmork");
+			c1.setCoordinates(coo);
+	
+			Cart c = new Cart();
+			c1.addCart(c);
+			
+			Product p1 = new Product();
+			p1.setName("Gouigoui rebondissant");
+			p1.setPrice(.5);
+			p1.setCategory(CategoryDAO.INSTANCE.getByName("Truc en plastoc"));
+			c.addContent(new Content(p1, 2));
+			c.addContent(new Content(ProductDAO.INSTANCE.getByName("Caribou mécanique"), 6));
+			
+			CRUDManager.createOrUpdate(c1);
+			
+			
+			System.out.println(ClientDAO.INSTANCE.getAllByLastName("Zorbier").toString());
+			
+			System.out.println(CartDAO.INSTANCE.getAllByPaymentState(false).toString());
+	
+			System.out.println("All is well.");
+		} catch(JCSIException e) {
+			CRUDManager.closeSession();
+		}
 	}
 	
 }
